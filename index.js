22aa57e76eb9f95cba64376220809a8f895bb39c
Israel-Brown <menelek7171@byui.edu>
2025-03-15 21:08:25 -0500

    def

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const swaggerDocument = require('./swagger.json');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // Environment variables
 const PORT = process.env.PORT || 3000;
